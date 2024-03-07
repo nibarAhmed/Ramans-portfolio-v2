400befa88a23dafe4d2b813cec1c852d8311995f
nibarsito <nibarsito2@gmail.com>
2024-03-07 22:38:23 +0100

    removed  skills from skills.ts and added c++ icon

--- data/Skills.ts
+++ data/Skills.ts
@@ -18,51 +18,16 @@ export const Frontend_skill = [
     height: 65,
   },
   {
-    skillName: "Tailwind CSS",
-    Image: "/logo/tailwind.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skillName: "Material UI",
-    Image: "/logo/mui.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skillName: "React",
-    Image: "/logo/react.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skillName: "Redux",
-    Image: "/logo/redux.png",
-    width: 80,
-    height: 80,
-  },
-
-  {
     skillName: "TypeScript",
     Image: "/logo/ts.png",
     width: 80,
     height: 80,
   },
-  {
-    skillName: "Nextjs ",
-    Image: "/logo/next.png",
-    width: 80,
-    height: 80,
-  },
+  
 ];
 
 export const Backend_skill = [
-  {
-    skillName: "Nodejs",
-    Image: "/logo/node-js.png",
-    width: 70,
-    height: 70,
-  },
+  
   {
     skillName: "Java",
     Image: "/logo/java.png",
@@ -70,22 +35,10 @@ export const Backend_skill = [
     height: 90,
   },
   {
-    skillName: "Python",
+     skillName: "Python",
     Image: "/logo/python.png",
     width: 70,
     height: 70,
-  },
-  {
-    skillName: "Expressjs",
-    Image: "/logo/express.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skillName: "Spring",
-    Image: "/logo/spring.png",
-    width: 90,
-    height: 90,
   },
   {
     skillName: "MySQL",
@@ -93,39 +46,10 @@ export const Backend_skill = [
     width: 70,
     height: 70,
   },
-  {
-    skillName: "Prisma",
-    Image: "/logo/prisma.webp",
-    width: 70,
-    height: 70,
-  },
-  {
-    skillName: "Sequelize",
-    Image: "/logo/sequelize.svg",
-    width: 70,
-    height: 70,
-  },
+
 ];
 
 export const Devops = [
-  {
-    skillName: "Azure",
-    Image: "/logo/azure.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skillName: "Github Actions",
-    Image: "/logo/git.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skillName: "Kubernetes",
-    Image: "/logo/kubernetes.png",
-    width: 70,
-    height: 70,
-  },
   {
     skillName: "Docker",
     Image: "/logo/docker.webp",
@@ -139,24 +63,7 @@ export const Devops = [
     width: 60,
     height: 60,
   },
-  {
-    skillName: "Nginx",
-    Image: "/logo/nginx.png",
-    width: 60,
-    height: 60,
-  },
-  {
-    skillName: "Grafana",
-    Image: "/logo/grafana_.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skillName: "Prometheus",
-    Image: "/logo/prometheus.svg",
-    width: 60,
-    height: 60,
-  },
+
 ];
 
 export const Other_skill = [
@@ -167,40 +74,4 @@ export const Other_skill = [
     height: 60,
   },
 
-  {
-    skillName: "Postman",
-    Image: "/logo/postman.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skillName: "Jest",
-    Image: "/logo/jest.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skillName: "Liquibase",
-    Image: "/logo/liquibase.png",
-    width: 120,
-    height: 120,
-  },
-  {
-    skillName: "DataGrip",
-    Image: "/logo/datagrip.svg",
-    width: 60,
-    height: 60,
-  },
-  {
-    skillName: "IntelliJ",
-    Image: "/logo/intellij.svg",
-    width: 60,
-    height: 60,
-  },
-  {
-    skillName: "Figma",
-    Image: "/logo/figma.png",
-    width: 50,
-    height: 50,
-  },
 ];
