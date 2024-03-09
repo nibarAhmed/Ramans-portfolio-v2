43d525d9fb729f22a155a3e1723318681e491379
nibarsito <nibarsito2@gmail.com>
2024-03-09 21:11:37 +0100

    removed old experiences from experience.ts and added my own

--- data/Experience.ts
+++ data/Experience.ts
@@ -1,74 +1,38 @@
 export const ExperienceData = [
   {
     id: "job-1",
-    type: "work",
-    date: "December 2022 - January 2023",
-    title: "Junior Developer at Bally's Skövde",
-    location: "Skovde, Sweden",
+    type: "internship",
+    date: "March 2021 - April 2021",
+    title: "Junior Developer at Farsight Tech Nordic AB ",
+    location: "Skövde, Sweden",
     description:
-      "Participated in pair programming, handling player security and inquiries, and expanded skills in new technologies.",
+      "Responsibilities included writing scripts, performing testing on products and helping writing code for active web development projects in type script and java script.",
   },
-  {
-    id: "job-2",
-    type: "work",
-    date: "June 2022 - July 2022",
-    title: "Junior Developer at Bally's Skövde",
-    location: "Skovde, Sweden",
-    description:
-      "Developed a massage application, designing the database and implementing backend logic.",
-  },
-  {
-    id: "job-3",
-    type: "work",
-    date: "Jan 2022 - June 2022",
-    title: "Full Stack Developer at Integrify Remote",
-    location: "Remote",
-    description:
-      "Completed a bootcamp focusing on JavaScript, React.js, TypeScript, and AWS, working on real-world projects.",
-  },
-  {
-    id: "job-4",
-    type: "work",
-    date: "June 2019 - April 2022",
-    title: "Truck Driver at Mathem",
-    location: "Gothenburg, Sweden",
-    description:
-      "Operated delivery routes, ensuring timely and accurate deliveries.",
-  },
-  {
-    id: "job-5",
-    type: "work",
-    date: "August 2017 - June 2018",
-    title: "Assembler at Volvo Cars",
-    location: "Gothenburg, Sweden",
-    description:
-      "Assembled vehicle parts, adhering to quality standards and production deadlines.",
-  },
-  {
-    id: "job-6",
-    type: "work",
-    date: "May 2016 - June 2018",
-    title: "Waiter at Scandic Hotels",
-    location: "Gothenburg, Sweden",
-    description: "Provided  customer service in a fast-paced environment.",
-  },
-
   // Education Experience
   {
     id: "edu-1",
     type: "education",
-    date: "August 2021 - May 2024",
+    date: "August 2021 - June 2024",
     title: "Bachelor's in Computer Science at University Skövde",
-    location: "Skovde, Sweden",
+    location: "Skövde, Sweden",
     description: "Specializing in System Development.",
   },
   {
     id: "edu-2",
     type: "education",
-    date: "August 2020 - Jan 2021",
-    title: "Economics Studies at University Borås",
-    location: "Boras, Sweden",
+    date: "August 2020 - June 2021",
+    title: "High school engineering degree , NTI Gymnasiet",
+    location: "Skövde, Sweden",
     description:
-      "Studied Economics for one term before deciding to pursue other interests.",
+      "TE4 at NTI is a one-year program focused on project development, offering participants a deep dive into software development methodologies. The curriculum emphasizes practical skills, complemented by internship opportunities with established companies to provide real-world experience.",
   },
+  {
+    id: "edu-3",
+    type: "education",
+    date: "August 2017 - June 2020",
+    title: "Highschool degree, the technology program, NTI Gymnasiet",
+    location: "Skövde, Sweden",
+    description: null
+  }
 ];
+ 
