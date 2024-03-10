4fd7ccb6b1f4296928bf42e41ff2fa67463cd5bf
nibarsito <nibarsito2@gmail.com>
2024-03-10 14:59:31 +0100

    added certification to certification.ts

--- data/Certification.ts
+++ data/Certification.ts
@@ -1,12 +1,14 @@
 export const CertificationData = [
   {
-    id: 1,
-    name: "no certifications yet",
-    date: "",
-    description: "",
-    CertificationLink: "",
-    backgroundImg: "/card-background/card-bg-4.jpg",
-    borderBackground: "/card-border/card-1.svg",
-    light: true,
-  },
+  id: 1,
+  name: "CCNAv7: Introduction to Networks",
+  date: "30/03/2022",
+  description:
+    "Proficient in configuring switches, routers, and troubleshooting connectivity for network security and resource access.",
+  CertificationLink:
+"https://www.linkedin.com/in/nibar-ahmed-4672b3258/details/certifications/1710078671515/single-media-viewer/?profileId=ACoAAD9nac8B2kwVOJ-cnWfJgyzATA-5jn2U9_w",
+  backgroundImg: "/card-background/card-bg-6.jpg",
+  borderBackground: "/card-border/card-4.svg",
+  light: true,
+}
   ];
